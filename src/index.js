29d9602d8a3641ce089e5c893d7d9e43ae609b9e
Brandon Vandegrift <bmv437@gmail.com>
2017-12-10 13:52:11 -0500

    remove Hello import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-import Hello from "./Hello";
 import { createStore, combineReducers } from "redux";
 import { reducer as formReducer, reduxForm } from "redux-form";
 import { Provider } from "react-redux";
